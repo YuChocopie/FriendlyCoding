209be3f80b1015df81b9b6e66c8fab3ec9c05f53
YuChocopie <rladbwjd341@naver.com>
2019-10-27 17:35:58 +0900

    Add character movement
    - 기기의 view크기를 구해 이동 간격 조절
    - PrincessViewModel.kt에서 주어진 만큼 일정간격 칸을 이동할 수 있게 한다
    
    Fixed #6

--- app/src/main/java/com/mashup/friendlycoding/MainActivity.kt
+++ app/src/main/java/com/mashup/friendlycoding/MainActivity.kt
@@ -1,49 +1,39 @@
 package com.mashup.friendlycoding
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.annotation.Nullable
+import android.view.View
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.mashup.friendlycoding.adapter.CodeBlockAdapter
 import com.mashup.friendlycoding.databinding.ActivityMainBinding
-import com.mashup.friendlycoding.repository.CodeBlock
-import com.mashup.friendlycoding.viewmodel.CodeBlockViewModel
-import kotlinx.android.synthetic.main.activity_main.*
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import androidx.databinding.Observable
+import com.mashup.friendlycoding.viewmodel.PrincessViewModel
 
 
 class MainActivity : AppCompatActivity() {
-    private val mCodeBlockViewModel = CodeBlockViewModel()
-    lateinit var mAdapter : CodeBlockAdapter
+
+    private var mSienaTestViewModel = PrincessViewModel()
+    lateinit var layoutMainView: View
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         binding.lifecycleOwner = this
-        binding.codeBlockVM = mCodeBlockViewModel
-        binding.codeBlock = mCodeBlockViewModel.getBlockButton()
 
-        //initialization
-        mCodeBlockViewModel.init()
+        layoutMainView = this.findViewById(R.id.constraintLayout)
+        binding.lifecycleOwner = this
+        binding.testSiena = mSienaTestViewModel
+        mSienaTestViewModel.setPrincessImage(binding.ivPrinsecess)
 
-        //recycler view connects
-        mAdapter = CodeBlockAdapter(this, mCodeBlockViewModel.getCodeBlock()?.value!!)
-        var linearLayoutManager = LinearLayoutManager(this)
-        rc_code_block_list.layoutManager = linearLayoutManager
-        rc_code_block_list.adapter = mAdapter
 
-        mCodeBlockViewModel.getCodeBlock()?.observe(this, object : Observer<List<CodeBlock>> {
-            override  fun onChanged(@Nullable codeBlock: List<CodeBlock>) {
-                mAdapter.notifyDataSetChanged()
-            }
-        })
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+
+        Log.e("Layout Width - ", "Width" + (layoutMainView.width))
+
+        mSienaTestViewModel.setViewSize(layoutMainView.width)
+
     }
 }
--- app/src/main/java/com/mashup/friendlycoding/viewmodel/PrincessViewModel.kt
+++ app/src/main/java/com/mashup/friendlycoding/viewmodel/PrincessViewModel.kt
@@ -1,7 +1,47 @@
 package com.mashup.friendlycoding.viewmodel
 
+import android.util.Log
+import android.widget.ImageView
 import androidx.lifecycle.ViewModel
 
-class PrincessViewModel : ViewModel(){
+class PrincessViewModel : ViewModel() {
+    private var princessImg: ImageView? = null
+    var xy = true
+    var unit = 1
+    var width = 0;
 
+    fun setPrincessImage(view: ImageView) {
+        this.princessImg = view
+    }
+
+    fun setViewSize(width: Int) =
+        { -> this.width = width}
+
+
+    fun go() {
+        if (princessImg != null)
+            if (xy)
+                changeX()
+            else
+                changeY()
+    }
+
+    fun rotation() {
+        xy = !xy
+        unit = if (!xy) -unit else unit
+    }
+
+    fun changeX() {
+        princessImg!!.x = (princessImg!!.x + width / 10 * unit)
+        Thread.sleep(100)
+        Log.e("123123", "" + princessImg!!.x)
+
+    }
+
+    fun changeY() {
+        princessImg!!.y = (princessImg!!.y + width / 10 * unit)
+        Thread.sleep(100)
+        Log.e("123123", "" + princessImg!!.y)
+
+    }
 }
