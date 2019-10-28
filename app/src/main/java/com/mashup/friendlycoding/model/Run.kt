604bf33e99d74bef939db3514510949e033813ee
YuChocopie <rladbwjd341@naver.com>
2019-10-28 22:02:17 +0900

    solved error

--- app/src/main/java/com/mashup/friendlycoding/model/Run.kt
+++ app/src/main/java/com/mashup/friendlycoding/model/Run.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import com.mashup.friendlycoding.repository.CodeBlock
 
 class Run {
-    var moveView = MutableLiveData<Int>()
-    var mCodeBlock = MutableLiveData<ArrayList<CodeBlock>>()
+    private var moveView = MutableLiveData<Int>()
+    private var mCodeBlock = MutableLiveData<ArrayList<CodeBlock>>()
 
     fun getCodeBlock(): LiveData<ArrayList<CodeBlock>> {
         return mCodeBlock
@@ -24,10 +24,9 @@ class Run {
         mCodeBlock.postValue(block)
     }
 
-    fun clearBlock() {
-        moveView.value = -1
+    fun deleteBlock(position : Int) {
         val block = mCodeBlock.value
-        mCodeBlock.value!!.clear()
+        mCodeBlock.value!!.removeAt(position)
         mCodeBlock.postValue(block)
     }
 
@@ -35,38 +34,22 @@ class Run {
         return moveView
     }
 
-    public inner class RunThead : Thread() {
-        override fun run() {
-            try {
-                for (i in 0 until mCodeBlock.value!!.size) {
-                    when (mCodeBlock.value!![i].funcName) {
-                        "move();" -> {
-                            moveView.postValue(0)
-                            Log.e("갑니다", "0")
-                            sleep(1000)
-                        }
-                        "turnLeft();" -> {
-                            //    moveView.value = 1
-                            moveView.postValue(1)
-                            Log.e("갑니다", "1")
-                            sleep(1000)
-                        }
-                        "turnRight();" -> {
-                            //  moveView.value = 2
-                            moveView.postValue(2)
-                            Log.e("갑니다", "2")
-                            sleep(1000)
-                        }
-                    }
+    fun run() {
+        for (i in 0 until mCodeBlock.value!!.size) {
+            when (mCodeBlock.value!![i].funcName) {
+                "move();" -> {
+                    moveView.value = 0
+                    Log.e("갑니다", "0")
                 }
-            } catch (e : IndexOutOfBoundsException) {
-                return
+                "turnLeft();" -> {
+                    moveView.value = 1
+                    Log.e("갑니다", "1")
+                }
+                "turnRight();" -> {
+                    moveView.value = 2
+                    Log.e("갑니다", "2")
+                }
             }
         }
-    }
-
-    fun run() {
-        val run = RunThead()
-        run.start()
     }
 }
