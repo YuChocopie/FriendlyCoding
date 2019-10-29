53714087ca99b411e581285282a9c199a26d1799
HyeongDo <gudeh8080@gmail.com>
2019-10-29 12:58:59 +0900

    recyclerview

--- app/src/main/java/com/mashup/friendlycoding/SelectStageActivity.kt
+++ app/src/main/java/com/mashup/friendlycoding/SelectStageActivity.kt
@@ -3,6 +3,7 @@ package com.mashup.friendlycoding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.mashup.friendlycoding.adapter.StageAdapter
 import com.mashup.friendlycoding.databinding.ActivityPlayBinding
 import com.mashup.friendlycoding.databinding.ActivitySelectStageBinding
 import com.mashup.friendlycoding.viewmodel.BattleViewModel
@@ -10,14 +11,24 @@ import com.mashup.friendlycoding.viewmodel.StageViewModel
 
 class SelectStageActivity : BaseActivity() {
 
-    private var mStageViewModel = StageViewModel()
+    //private var mStageViewModel = StageViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_stage)
         val binding = DataBindingUtil.setContentView<ActivitySelectStageBinding>(this, R.layout.activity_select_stage)
         binding.lifecycleOwner = this
-        binding.stageVM = mStageViewModel
+        //binding.stageVM = mStageViewModel
+
+        val list = ArrayList<StageViewModel>()
+        list.add(StageViewModel(getDrawable(R.drawable.cave)!!))
+        list.add(StageViewModel(getDrawable(R.drawable.cave)!!))
+        list.add(StageViewModel(getDrawable(R.drawable.cave)!!))
+        list.add(StageViewModel(getDrawable(R.drawable.cave)!!))
+        list.add(StageViewModel(getDrawable(R.drawable.cave)!!))
+
+        val adapter = StageAdapter(list)
+        binding.recyclerViewStage.adapter = adapter
 
     }
 }
