e4bb0552a50bd4038bae6dc85ccf2584f140eaee
HyeongDo <gudeh8080@gmail.com>
2019-11-18 07:11:52 +0900

    변수 색 추가

--- app/src/main/java/com/mashup/friendlycoding/activity/PlayActivity.kt
+++ app/src/main/java/com/mashup/friendlycoding/activity/PlayActivity.kt
@@ -1,5 +1,6 @@
 package com.mashup.friendlycoding.activity
 
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
@@ -75,19 +76,19 @@ class PlayActivity : BaseActivity() {
 
         when(stageNum){
             21 -> {
-                binding.tvCount.setText("Count : ")
+                binding.tvCount.setText("Count = ")
                 binding.tvCountSet.setText("${mRun.mPrincess.branchCnt}")
                 binding.tvState.setText("Book = ")
                 binding.tvStateSet.setText("${mRun.mPrincess.isBook}")
             }
             22 -> {
-                binding.tvCount.setText("Count : ")
+                binding.tvCount.setText("Count = ")
                 binding.tvCountSet.setText("${mRun.mPrincess.mushroomCnt}")
                 binding.tvState.setText("Mushroom = ")
                 binding.tvStateSet.setText("${mRun.mPrincess.isMushroom}")
             }
             23 -> {
-                binding.tvCount.setText("Count : ")
+                binding.tvCount.setText("Count = ")
                 binding.tvCountSet.setText("${mRun.mPrincess.branchCnt}")
                 binding.tvState.setText("Branch = ")
                 binding.tvStateSet.setText("${mRun.mPrincess.isBranch}")
