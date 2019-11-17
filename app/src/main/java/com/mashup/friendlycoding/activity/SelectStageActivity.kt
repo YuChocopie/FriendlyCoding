9133164efa761e523185bee1d5ad25efd8a46905
YuChocopie <rladbwjd341@naver.com>
2019-11-18 08:10:06 +0900

    Select Stage 수정

--- app/src/main/java/com/mashup/friendlycoding/activity/SelectStageActivity.kt
+++ app/src/main/java/com/mashup/friendlycoding/activity/SelectStageActivity.kt
@@ -1,7 +1,6 @@
 package com.mashup.friendlycoding.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.mashup.friendlycoding.R
@@ -14,19 +13,19 @@ class SelectStageActivity : BaseActivity() {
 
         val actNum = intent.getIntExtra("actNum", 1)
 
-        act_title.text = "액트 " + (actNum/10).toString()
+        tvActTitle.text = "액트 " + (actNum/10).toString()
 
         val intent = Intent(this, PlayActivity::class.java)
         val listener = View.OnClickListener{
             when (it.id) {
-                R.id.stage1 -> intent.putExtra("stageNum", actNum+1)
-                R.id.stage2 -> intent.putExtra("stageNum", actNum+2)
-                R.id.stage3 -> intent.putExtra("stageNum", actNum+3)
+                R.id.ivActStage1 -> intent.putExtra("stageNum", actNum+1)
+                R.id.ivActStage2 -> intent.putExtra("stageNum", actNum+2)
+                R.id.ivActStage3 -> intent.putExtra("stageNum", actNum+3)
             }
             startActivity(intent)
         }
-        stage1.setOnClickListener(listener)
-        stage2.setOnClickListener(listener)
-        stage3.setOnClickListener(listener)
+        ivActStage1.setOnClickListener(listener)
+        ivActStage2.setOnClickListener(listener)
+        ivActStage3.setOnClickListener(listener)
     }
 }
