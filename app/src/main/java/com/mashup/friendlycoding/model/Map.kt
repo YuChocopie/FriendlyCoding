af27785c89548db71692a7ac10116125a5e1bf39
YuChocopie <rladbwjd341@naver.com>
2019-11-28 07:18:56 +0900

    .

--- app/src/main/java/com/mashup/friendlycoding/model/Map.kt
+++ app/src/main/java/com/mashup/friendlycoding/model/Map.kt
@@ -2,7 +2,7 @@ package com.mashup.friendlycoding.model
 
 import android.util.Log
 
-class Map (var mapList : Array<Array<Int>>? = null, var drawables: MapDrawable? = null, val startX : Int = 0, val startY : Int = 9,var stageNum:Int=10) {
+class Map (var mapList : Array<Array<Int>>? = null, var drawables: MapDrawable? = null, val startX : Int = 0, val startY : Int = 9) {
     fun itemPicked(y: Int, x: Int) {
         Log.e("이미지삭제","($x, $y)")
 //        this.mapList!![y][x] = 0
